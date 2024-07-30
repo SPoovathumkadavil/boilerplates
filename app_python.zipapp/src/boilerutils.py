fccd3930cca5aaa16b06e922089d1cceec92d19d
Sally <spoovathumkadavil16@claytonschools.net>
2024-07-30 09:17:39 -0500

    python fix dep and conf dir

--- app_python.zipapp/src/boilerutils.py
+++ app_python.zipapp/src/boilerutils.py
@@ -5,7 +5,9 @@ def get_home_dir():
     return os.path.expanduser("~")
 
 
+APP_NAME = "app"
+
 HOME_DIR = get_home_dir()
 DEV_DIR = os.path.join(HOME_DIR, "dev")
-DEP_DIR = os.path.join(DEV_DIR, ".dependencies")
-CONF_DIR = os.path.join(DEV_DIR, ".config")
+DEP_DIR = os.path.join(DEV_DIR, ".dependencies", APP_NAME)
+CONF_DIR = os.path.join(DEV_DIR, ".config", APP_NAME)
