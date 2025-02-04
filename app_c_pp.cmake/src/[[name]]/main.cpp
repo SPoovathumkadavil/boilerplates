96be0f2cf35f13ea76d6c0b3319b40f353e905a5
sally <spoovathumkadavil16@claytonschools.net>
2025-02-04 11:48:25 -0600

    added print_help to cmake app

--- app_c_pp.cmake/src/[[name]]/main.cpp
+++ app_c_pp.cmake/src/[[name]]/main.cpp
@@ -3,7 +3,11 @@
 
 #include <fetalib/cli/arguments.hpp>
 
-int main(int argc, char **argv)
+void print_help(library *lib, feta::ArgumentParser *arg_parser) {
+  for (std::string s : arg_parser->get_help_message(lib->_name, true, 80)) std::printf("%s\n", s.c_str());
+}
+
+auto main(int argc, char **argv) -> int
 {
   library lib = library {};
 
@@ -11,12 +15,17 @@ int main(int argc, char **argv)
   feta::Argument a_test = feta::get_blank_argument()
                             .withKey("-t")
                             .withAlternateKey("--test")
+                            .withHelpMessage("Enables test mode.")
                             .withFlag();
+  feta::ArgumentDependency c_help = feta::get_blank_argument_dependency()
+                            .withKey("help")
+                            .withHelpMessage("Prints the help message.");
   argparser.add(&a_test);
+  argparser.add(&c_help);
 
   if (argparser.get<bool>("-t").value()) lib._test = true;
-
-  std::cout << "is test: " << lib._test << '\n';
+  if (argparser.get_command()->key == "help") print_help(&lib, &argparser);
 
   return 0;
 }
+
